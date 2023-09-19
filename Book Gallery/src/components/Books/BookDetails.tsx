b833cf1ff5460db668d1e594ea6625a8edb29491
moshawwa <mohammedshawa10@gmail.com>
2023-09-19 18:16:01 +0300

    edit details for book cards

--- Book Gallery/src/components/Books/BookDetails.tsx
+++ Book Gallery/src/components/Books/BookDetails.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ApiClient from "../../services/ApiClient";
+import ApiClient, { baseUrl } from "../../services/ApiClient";
 import { Book } from "../../types/Book";
 
 const BookDetails = () => {
@@ -14,18 +14,58 @@ const BookDetails = () => {
       setBook(data);
       setIsLoading(false);
     });
-  }, []);
+  }, [bookId]);
 
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            {isLoading && <p>Loading book..</p>}
-            {book && <p>{book.name}</p>}
+      {isLoading ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {/* Loading placeholders */}
+              <div className="placeholder-container">
+                {[1, 2, 3, 4, 5, 6].map((index) => (
+                  <span key={index} className="placeholder col-12"></span>
+                ))}
+                <span className="placeholder" style={{ width: 25 }}></span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 p-5">
+              <div
+                className="card mb-3"
+                style={{ maxWidth: "1000px", height: "auto" }}
+              >
+                <div className="row g-0">
+                  <div className="col-4 col-md-4">
+                    <img
+                      src={`${baseUrl}/images/thumbs/med/${book.image}`}
+                      className="img-fluid rounded-start"
+                    />
+                  </div>
+                  <div className="col-8 col-md-8">
+                    <div className="card-body">
+                      <h5>{book.name}</h5>
+                      {book.author && <h5>by {book.author.name}</h5>}
+                      {book.category && (
+                        <h5 className="card-title">{book.category.name}</h5>
+                      )}
+                      <p className="card-text"> {book.publishYear}</p>
+
+                      <h5>${book.price}</h5>
+                      <small>{book.about}</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
