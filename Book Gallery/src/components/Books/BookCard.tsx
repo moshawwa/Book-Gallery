7232d50a2e4b8375a5f5865b77f9abb638dfea73
moshawwa <mohammedshawa10@gmail.com>
2023-09-19 18:21:46 +0300

    edit loading in book card

--- Book Gallery/src/components/Books/BookCard.tsx
+++ Book Gallery/src/components/Books/BookCard.tsx
@@ -20,23 +20,79 @@ const BookCard = () => {
     <>
       {bookIsLoading ? (
         <>
-          <div className="card" aria-hidden="true">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title placeholder-glow">
-                <span className="placeholder col-6"></span>
-              </h5>
-              <p className="card-text placeholder-glow">
-                <span className="placeholder col-7"></span>
-                <span className="placeholder col-4"></span>
-                <span className="placeholder col-4"></span>
-                <span className="placeholder col-6"></span>
-                <span className="placeholder col-8"></span>
-              </p>
-              <a
-                className="btn btn-primary disabled placeholder col-6"
-                aria-disabled="true"
-              ></a>
+          <div className="container text-center">
+            <div className="row">
+              <div className="col">
+                {" "}
+                <div className="card" aria-hidden="true">
+                  <img src="..." className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                      <span className="placeholder col-6"></span>
+                    </h5>
+                    <p className="card-text placeholder-glow">
+                      <span className="placeholder col-7"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-6"></span>
+                      <span className="placeholder col-8"></span>
+                    </p>
+                  </div>
+                </div>{" "}
+              </div>
+              <div className="col">
+                {" "}
+                <div className="card" aria-hidden="true">
+                  <img src="..." className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                      <span className="placeholder col-6"></span>
+                    </h5>
+                    <p className="card-text placeholder-glow">
+                      <span className="placeholder col-7"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-6"></span>
+                      <span className="placeholder col-8"></span>
+                    </p>
+                  </div>
+                </div>{" "}
+              </div>
+              <div className="col">
+                <div className="card" aria-hidden="true">
+                  <img src="..." className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                      <span className="placeholder col-6"></span>
+                    </h5>
+                    <p className="card-text placeholder-glow">
+                      <span className="placeholder col-7"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-6"></span>
+                      <span className="placeholder col-8"></span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                {" "}
+                <div className="card" aria-hidden="true">
+                  <img src="..." className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                      <span className="placeholder col-6"></span>
+                    </h5>
+                    <p className="card-text placeholder-glow">
+                      <span className="placeholder col-7"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-6"></span>
+                      <span className="placeholder col-8"></span>
+                    </p>
+                  </div>
+                </div>{" "}
+              </div>
             </div>
           </div>
         </>
@@ -78,3 +134,5 @@ const BookCard = () => {
 };
 
 export default BookCard;
+{
+}
