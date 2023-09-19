7ba72fadbf32eeacc43a780d6bc2f9d81bf4a4a7
moshawwa <mohammedshawa10@gmail.com>
2023-09-19 16:50:11 +0300

    remove category

--- Book Gallery/src/components/Books/BookCard.tsx
+++ Book Gallery/src/components/Books/BookCard.tsx
@@ -5,44 +5,74 @@ import { Book } from "../../types/Book";
 
 const BookCard = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [bookIsLoading, setBookIsLoading] = useState(false);
 
   useEffect(() => {
     ApiClient.get<Book[]>("/Book").then((response) => {
       console.log(response.data);
+      // setBookIsLoading(false);
       setBooks(response.data);
+      setBookIsLoading(false);
     });
+    setBookIsLoading(true);
   }, []);
   return (
     <>
-      <div className="row">
-        {books.map((book) => {
-          return (
-            <div key={book.id} className="col-md-3">
-              <div
-                className="card"
-                style={{ backgroundColor: "#f0f0f0", padding: "10px" }}
-              >
-                <img
-                  src={`${baseUrl}/images/thumbs/med/${book.image}`}
-                  className="card-img-top"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <Link
-                    style={{ textDecoration: "none", color: "blue" }}
-                    to={`/Books/${book.id}`}
-                  >
-                    {book.name}
-                  </Link>
-                  <p className="card-text" style={{ color: "green" }}>
-                    {book.price}
-                  </p>
-                </div>
-              </div>
+      {bookIsLoading ? (
+        <>
+          <div className="card" aria-hidden="true">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title placeholder-glow">
+                <span className="placeholder col-6"></span>
+              </h5>
+              <p className="card-text placeholder-glow">
+                <span className="placeholder col-7"></span>
+                <span className="placeholder col-4"></span>
+                <span className="placeholder col-4"></span>
+                <span className="placeholder col-6"></span>
+                <span className="placeholder col-8"></span>
+              </p>
+              <a
+                className="btn btn-primary disabled placeholder col-6"
+                aria-disabled="true"
+              ></a>
             </div>
-          );
-        })}
-      </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="row">
+            {books.map((book) => {
+              return (
+                <div key={book.id} className="col-md-3">
+                  <div
+                    className="card"
+                    style={{ backgroundColor: "#f0f0f0", padding: "10px" }}
+                  >
+                    <img
+                      src={`${baseUrl}/images/thumbs/med/${book.image}`}
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <Link
+                        style={{ textDecoration: "none", color: "blue" }}
+                        to={`/Books/${book.id}`}
+                      >
+                        {book.name}
+                      </Link>
+                      <p className="card-text" style={{ color: "green" }}>
+                        {book.price}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </>
   );
 };
